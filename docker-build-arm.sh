d256b3ae842e9e6d7dcfaee5dadeacd8e98b05f9
General-Meow <spl.ivalis@gmail.com>
2017-01-22 20:51:50 +0000

    update to docker

--- docker-build-arm.sh
+++ docker-build-arm.sh
@@ -2,5 +2,5 @@
 rm ./files/mongodb-arm.tar.gz
 mkdir ./files/mongo-arm-extract
 cat ./files/mongodb-ubuntu-arm/mongodb-ubuntu-arm-3.4.1.tar.gz* > ./files/mongodb-arm.tar.gz
-tar -zxvf ./files/mongodb-arm.tar.gz -c ./files/mongo-arm-extract
+tar -zxvf ./files/mongodb-arm.tar.gz -C ./files/mongo-arm-extract
 docker build -f Dockerfile-arm -t generalmeow/mongodb:1.0-arm ./files
